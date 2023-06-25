731d672f1a5ac878a10de3fbf4e89c3f14be1b0f
osandrey <osandreyman@gmail.com>
2023-06-25 15:23:28 +0100

    Done

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
-from src.routes import contacts
+from src.routes import contacts, auth
 
-app = FastAPI()
 
+app = FastAPI(title="OSA-SWAGGER", swagger_ui_parameters={"operationsSorter": "method"})
+
+app.include_router(auth.router, prefix='/api')
 app.include_router(contacts.router, prefix='/api')
 
-@app.get("/")
+
+@app.get("/", tags=["Root"])
 def read_root():
     return {"message": "Hello World"}
